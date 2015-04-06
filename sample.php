aadf2595b7419d9250a793337cb5ba29e0e0844b
Jordan Lev <github@jordanlev.com>
2015-04-06 14:26:01 -0500

    clean up php tags in sample.php markup

--- sample.php
+++ sample.php
@@ -87,125 +87,125 @@ require 'Form.php';
 //display the form...
 ?>
 
-	<?php if ($form->errors): /* re-display form with validation errors... */ ?>
+	<?php if ($form->errors) { /* re-display form with validation errors... */ ?>
 		<ul class="errors">
-			<?php foreach ($form->errors as $error): ?>
-				<li><?= $error ?></li>
-			<?php endforeach; ?>
+			<?php foreach ($form->errors as $error) { ?>
+				<li><?=$error?></li>
+			<?php } ?>
 		</ul>
-	<?php endif ?>
+	<?php } ?>
 
 	<style>.error { color: red; }</style>
 
 	<form action="" method="post" enctype="multipart/form-data">
 
-		<div class="<?= $form->error('email', 'error') ?>">
+		<div class="<?=$form->error('email', 'error')?>">
 			<label for="email">Email</label>
 			<div>
-				<input type="text" id="email" name="email" value="<?= h($form->email) ?>">
+				<input type="text" id="email" name="email" value="<?=h($form->email)?>">
 			</div>
-			<?php if ($form->error('email')): ?>
+			<?php if ($form->error('email')) { ?>
 				<div class="error">
-					<?= $form->error('email') ?>
+					<?=$form->error('email')?>
 				</div>
-			<?php endif ?>
+			<?php } ?>
 		</div>
 
-		<div class="<?= $form->error('password', 'error') ?>">
+		<div class="<?=$form->error('password', 'error')?>">
 			<label for="password">Password</label>
 			<div>
-				<input type="password" id="password" name="password" value="<?= h($form->password) ?>">
+				<input type="password" id="password" name="password" value="<?=h($form->password)?>">
 			</div>
-			<?php if ($form->error('password')): ?>
+			<?php if ($form->error('password')) { ?>
 				<div class="error">
-					<?= $form->error('password') ?>
+					<?=$form->error('password')?>
 				</div>
-			<?php endif ?>
+			<?php } ?>
 		</div>
 
-		<div class="<?= $form->error('password_confirmation', 'error') ?>">
+		<div class="<?=$form->error('password_confirmation', 'error')?>">
 			<label for="password_confirmation">Confirm</label>
 			<div>
-				<input type="password" id="password_confirmation" name="password_confirmation" value="<?= h($form->password_confirmation) ?>">
+				<input type="password" id="password_confirmation" name="password_confirmation" value="<?=h($form->password_confirmation)?>">
 			</div>
-			<?php if ($form->error('password_confirmation')): ?>
+			<?php if ($form->error('password_confirmation')) { ?>
 				<div class="error">
-					<?= $form->error('password_confirmation') ?>
+					<?=$form->error('password_confirmation')?>
 				</div>
-			<?php endif ?>
+			<?php } ?>
 		</div>
 
-		<div class="<?= $form->error('picture', 'error') ?>">
+		<div class="<?=$form->error('picture', 'error')?>">
 			<label for="picture">Picture</label>
 			<div>
 				<input type="file" id="picture" name="picture">
 			</div>
-			<?php if ($form->error('picture')): ?>
+			<?php if ($form->error('picture')) { ?>
 				<div class="error">
-					<?= $form->error('picture') ?>
+					<?=$form->error('picture')?>
 				</div>
-			<?php endif ?>
+			<?php } ?>
 		</div>
 
-		<div class="<?= $form->error('topic', 'error') ?>">
+		<div class="<?=$form->error('topic', 'error')?>">
 			<label for="topic">Topic</label>
 			<div>
 				<select id="topic" name="topic">
-					<?= $form->options('topic', array(
+					<?=$form->options('topic', array(
 						'' => '--Choose One--',
 						'first' => 'First Topic',
 						'second' => 'Sectond Topic',
 						'third' => 'Yet Another One',
-					)) ?>
+					))?>
 				</select>
 			</div>
-			<?php if ($form->error('topic')): ?>
+			<?php if ($form->error('topic')) { ?>
 				<div class="error">
-					<?= $form->error('topic') ?>
+					<?=$form->error('topic')?>
 				</div>
-			<?php endif ?>
+			<?php } ?>
 		</div>
 
 		<div>
 			<label for="comments">Comments (optional):</label>
-			<textarea id="comments" name="comments"><?= h($form->comments) ?></textarea>
+			<textarea id="comments" name="comments"><?=h($form->comments)?></textarea>
 		</div>
 
-		<div class="<?= $form->error('agree_to_terms', 'error') ?>">
+		<div class="<?=$form->error('agree_to_terms', 'error')?>">
 			<div>
 				<label>
-					<input type="checkbox" id="agree_to_terms" name="agree_to_terms" value="1" <?= $form->agree_to_terms ? 'checked' : '' ?>>
+					<input type="checkbox" id="agree_to_terms" name="agree_to_terms" value="1" <?=($form->agree_to_terms ? 'checked' : '')?>>
 					I agree to the terms &amp; conditions
 				</label>
 			</div>
-			<?php if ($form->error('agree_to_terms')): ?>
+			<?php if ($form->error('agree_to_terms')) { ?>
 				<div class="error">
-					<?= $form->error('agree_to_terms') ?>
+					<?=$form->error('agree_to_terms')?>
 				</div>
-			<?php endif ?>
+			<?php } ?>
 		</div>
 
 		<div>
 			<label>
-				<input type="checkbox" name="misc_choices[7]" value="1" <?= empty($form->misc_choices[7]) ? '' : 'checked' ?>>
+				<input type="checkbox" name="misc_choices[7]" value="1" <?=(empty($form->misc_choices[7]) ? '' : 'checked')?>>
 				Test Item #7
 			</label>
 			<br>
 
 			<label>
-				<input type="checkbox" name="misc_choices[9]" value="1" <?= empty($form->misc_choices[9]) ? '' : 'checked' ?>>
+				<input type="checkbox" name="misc_choices[9]" value="1" <?=(empty($form->misc_choices[9]) ? '' : 'checked')?>>
 				Test Item #9
 			</label>
 			<br>
 
 			<label>
-				<input type="checkbox" name="misc_choices[15]" value="1" <?= empty($form->misc_choices[15]) ? '' : 'checked' ?>>
+				<input type="checkbox" name="misc_choices[15]" value="1" <?=(empty($form->misc_choices[15]) ? '' : 'checked')?>>
 				Test Item #15
 			</label>
 			<br>
 
 			<label>
-				<input type="checkbox" name="misc_choices[pizza]" value="1" <?= empty($form->misc_choices['pizza']) ? '' : 'checked' ?>>
+				<input type="checkbox" name="misc_choices[pizza]" value="1" <?=(empty($form->misc_choices['pizza']) ? '' : 'checked')?>>
 				I like pizza!
 			</label>
 			<br>
